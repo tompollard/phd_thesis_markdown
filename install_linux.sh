3434337be763de639227f3ca7f1d7f20381731e0
Robin Cremer <robincremer3@gmail.com>
2021-02-24 13:40:04 +0100

    modified install script to be run with https://github.com/scottkosty/install-tl-ubuntu

--- install_linux.sh
+++ install_linux.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
 
 set -e  # makes the script fail as soon as one command fails
+# removed sudo as an alias was used in bash.rc and in session: alias sudo='sudo env PATH=$PATH'
 
-sudo tlmgr install truncate
-sudo tlmgr install tocloft
-sudo tlmgr install wallpaper
-sudo tlmgr install morefloats
-sudo tlmgr install sectsty
-sudo tlmgr install siunitx
-sudo tlmgr install threeparttable
-sudo tlmgr update l3packages
-sudo tlmgr update l3kernel
-sudo tlmgr update l3experimental
-sudo tlmgr update l3backend
+tlmgr install truncate
+tlmgr install tocloft
+tlmgr install wallpaper
+tlmgr install morefloats
+tlmgr install sectsty
+tlmgr install siunitx
+tlmgr install threeparttable
+tlmgr update l3packages
+tlmgr update l3kernel
+tlmgr update l3experimental
+tlmgr update l3backend
+# to be installed too:
+# mathspec
+# xltxtra
+# realscripts
+# eso-pic
+# ctable
+# listings
+# pdfpages
+# bbold
+# cleveref
+# 
 # Would it be simpler to just update all packages? (takes ~10m)
 # sudo tlmgr update --all
 pip install pandoc-fignos pandoc-eqnos pandoc-tablenos pandoc-secnos pandoc-shortcaption
